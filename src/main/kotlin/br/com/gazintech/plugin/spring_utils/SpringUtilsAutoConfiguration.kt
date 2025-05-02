24f81185c5d4d684e891fac8e26a7e8d048fa026
Luan Gazin <luan_gazin@yahoo.com.br>
2025-05-02 17:42:13 -0300

    feat:preparing spring module configuration

---
+++ src/main/kotlin/br/com/gazintech/plugin/spring_utils/SpringUtilsAutoConfiguration.kt
@@ -0,0 +1,9 @@
+package br.com.gazintech.plugin.spring_utils
+
+import br.com.gazintech.plugin.spring_utils.idempotency.IdempotencyConfiguration
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@ImportAutoConfiguration(classes = [IdempotencyConfiguration::class])
+class SpringUtilsAutoConfiguration {}
